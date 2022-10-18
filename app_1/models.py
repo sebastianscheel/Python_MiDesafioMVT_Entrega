82a81799e8ceede29ab015f8f0e301ab3226672c
Sebas Scheel <jseba.scheel@gmail.com>
2022-10-17 23:53:32 -0300

    Se corrigio nombre BD - Estado Inicial 0

--- app_1/models.py
+++ app_1/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class familiar(models.Model):
+class Familiares(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=40)
     fecha_nacimiento = models.DateField()
