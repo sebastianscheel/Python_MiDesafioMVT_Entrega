bf5b22bfb9e08748583cef51a9eaa2fc1742f500
Sebas Scheel <jseba.scheel@gmail.com>
2022-10-17 23:46:24 -0300

    Se creo BD - Estado Inicial 0

---
+++ app_1/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class familiar(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    fecha_nacimiento = models.DateField()
+    telefono = models.IntegerField()
+    correo = models.EmailField()
